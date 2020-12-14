3504f9a8098b79944e348d4871fd2fa2cc8a6d19
markvong <mark.m.vong@gmail.com>
2020-12-13 23:07:17 -0800

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,35 @@
 var express = require("express");
 var router = express.Router();
 var fs = require("fs");
+var app = express();
+const serverless = require("serverless-http");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+// router.get("/", function (req, res, next) {
+//   res.render("index", { title: "Express" });
+// });
 
-router.get("/api", (req, res) => {
+router.get("/", (req, res) => {
   fs.readFile(__dirname + "/db.json", (err, json) => {
-    let obj = JSON.parse(json);
-    res.json(obj);
+    // let obj = JSON.parse(json);
+    // console.log(json);
+    res.json({
+      members: [
+        {
+          memberId: 1234,
+          firstName: "Mark",
+          lastName: "Vong"
+        },
+        {
+          memberId: 12345,
+          firstName: "Meek",
+          lastName: "Vong"
+        }
+      ]
+    });
   });
 });
 
-module.exports = router;
+app.use("/.netlify/functions/index", router);
+
+module.exports.handler = serverless(app);
